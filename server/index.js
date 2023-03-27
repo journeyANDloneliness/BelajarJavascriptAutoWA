54caa5dcdf146452abf26a5037d951f46c43a00a
Rumetta-may <achmad7actaca@gmail.com>
2023-03-27 12:11:02 +0700

    heroku again

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 import  express  from 'express';
+import {doc, koneksiSheet} from "love-utility"
 const app = express();
+await koneksiSheet()
 
 app.get('/', (req, res) => {
+	doc.sheetByTitle
   res.send('Hello, world!');
 });
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ const client = new ApolloClient({
 });
 
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <ApolloProvider client={client}>
     <App />
